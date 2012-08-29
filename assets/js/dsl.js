d730b475b2afb9d7a50879af15b7fc2caae47bf0
libra guest <thomas.schnocklake@gmail.com>
2012-08-29 06:43:55 -0400

    bd

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,49 +1,41 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Storage Example</title>
-
-    <script type="text/javascript" charset="utf-8" src="cordova-2.0.0.js"></script>
-    <script type="text/javascript" charset="utf-8">
-
-    // Wait for Cordova to load
-    //
-    document.addEventListener("deviceready", onDeviceReady, false);
-
-    // Cordova is ready
-    //
-    function onDeviceReady() {
-        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-        db.transaction(populateDB, errorCB, successCB);
+// the app method accepts a fn to invoke on init unobtrusively 
+var run = function(application) {
+    if (navigator.userAgent.indexOf('Browzr') > -1) {
+        // blackberry
+        setTimeout(application, 250)	
+    } else {
+        // attach to deviceready event, which is fired when phonegap is all good to go.
+        x$(document).on('deviceready', application, false);
     }
+}
 
-    // Populate the database 
-    //
-    function populateDB(tx) {
-         tx.executeSql('DROP TABLE IF EXISTS DEMO');
-         tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-         tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-         tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+// throw our settings into a lawnchair
+, store = new Lawnchair({adaptor:'dom'})
+
+// shows id passed
+, display = function(id) {
+    x$(["#welcome", "#map", "#settings"]).each(function(e, i) {
+        var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
+        x$(e).css({ 'display':display })
+    });
+}
+
+// reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
+, when = function(id, callback) {
+    x$(id + '_button').on('touchstart', function () {
+        if (x$(id).length > 0)
+            display(id);
+        if (callback)
+            callback.call(this);
+		return false;
+    });
+}
+
+// gets the value of the setting from the ui
+, ui = function(setting) {
+    var radio = x$('#settings_form')[0][setting];
+    for (var i = 0, l = radio.length; i < l; i++) {
+        if (radio[i].checked)
+            return radio[i].value;
     }
-
-    // Transaction error callback
-    //
-    function errorCB(tx, err) {
-        alert("Error processing SQL: "+err);
-    }
-
-
-
-    // Transaction success callback
-    //
-    function successCB() {
-        alert("success!");
-    }
-
-    </script>
-  </head>
-  <body>
-    <h1>Example</h1>
-    <p>Database</p>
-  </body>
-</html>
+};
